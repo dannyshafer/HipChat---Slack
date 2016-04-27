1176b42af3b975db78f99918d7ac2c66be36db65
Daniel Shafer <github@danielshafer.com>
2016-04-27 10:25:21 -0700

    finish send to hipchat

--- app/models/send_to_hipchat.rb
+++ app/models/send_to_hipchat.rb
@@ -1,13 +1,7 @@
 class SendToHipchat 
 	def initialize(message, username)
 		api_token = ENV['HIPCHAT_API_TOKEN']
-		client = HipChat::Client.new(api_token)
+		client = HipChat::Client.new(api_token, :api_version => 'v2')
 		client['Engineering'].send(username, message)
-		# @uri = ENV['SLACK_INCOMING_WEBHOOK_URL'] #this goes to engineering channel
-		# @poster = Slack::Poster.new(@uri)
-		# @poster.username = username
-		# @poster.icon_emoji = [':bananadance:', ':facepalm:', ':flying-money:', ':liftoff:', ':loading:', ':partyparrot:', ':partyparrot-shuffle:', ':plusone:', ':slowclap:', ':waving:', ':beers:'].sample
-		# @poster.send_message(message)
-		puts "yay"*100
 	end
 end
