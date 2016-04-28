e5eea8aa6164a8eefafc6882ffd22bb43c7d62f1
Daniel Shafer <github@danielshafer.com>
2016-04-28 14:24:59 -0700

    update for outgoing webhooks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def slack
+  	puts "omfg it works"*1000
+  end
+
+  def hipchat
+  	puts "omfg it works"*1000
+  end
 end
